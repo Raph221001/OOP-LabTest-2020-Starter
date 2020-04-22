88e91bf5a8771bdddf4b3f918ca753e7994b1f2f
Raph2210 <raphofeimu@gmail.com>
2020-04-22 13:42:14 +0100

    Created tasks.java

--- java/src/ie/tudublin/Tasks.java
+++ java/src/ie/tudublin/Tasks.java
@@ -1,12 +1,16 @@
 package ie.tudublin;
 
+import processing.core.PApplet;
 import processing.data.TableRow;
 
+//Create public class called task
 public class Tasks {
+    //Creating variables
     private String Tasks;
     private int Starttime;
     private int Endtime;
 
+    //Use Getter and Setter for variables
     public String getTasks() {
         return Tasks;
     }
@@ -30,6 +34,8 @@ public class Tasks {
     public void setEnd(int endtime) {
         Endtime = endtime;
     }
+
+    //Task Constructor with parameters
     public Tasks(String tasks, int starttime, int endtime){
         this.Tasks = tasks;
         this.Starttime = starttime;
@@ -37,5 +43,9 @@ public class Tasks {
     }
     public Tasks(TableRow tr){
         this(tr.getString("Task"), tr.getInt("Start"), tr.getInt("End"));
+    }
+
+    public static void main (String...args){
+        PApplet.main("ie.tudublin.Tasks");
     }
 }
