a60285417983775a9d4ad2745dfcdd549644f931
Raph2210 <raphofeimu@gmail.com>
2020-04-22 13:53:24 +0100

    Array list declared and print and load tasks done

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -1,43 +1,53 @@
 package ie.tudublin;
 
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
+
+import java.util.ArrayList;
 
 public class Gantt extends PApplet
-{	
-	
-	public void settings()
-	{
+{
+	//ArrayList created
+	ArrayList<Tasks> tasks = new ArrayList<Tasks>();
+
+	public void settings() {
 		size(800, 600);
 	}
 
-	public void loadTasks()
-	{
-		
+	//Loading tasks from csv file and stores in array
+	public void loadTasks() {
+		Table table = loadTable("tasks.csv", "header");
+		for(TableRow row:table.rows()) {
+			Tasks tr = new Tasks(row);
+			tasks.add(tr);
+		}
 	}
 
-	public void printTasks()
-	{
-		
-	}
-	
-	public void mousePressed()
-	{
-		println("Mouse pressed");	
+	//Prints tasks
+	public void printTasks() {
+		for(Tasks tr:tasks)
+		{
+			println(tr);
+		}
 	}
 
-	public void mouseDragged()
-	{
+	public void mousePressed() {
+		println("Mouse pressed");
+	}
+
+	public void mouseDragged() {
 		println("Mouse dragged");
 	}
 
-	
-	
-	public void setup() 
-	{
+
+
+	public void setup() {
+
 	}
-	
-	public void draw()
-	{			
+
+	public void draw() {
 		background(0);
 	}
 }
+
